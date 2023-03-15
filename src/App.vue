7e2927e5f2fca005da590a54d950cff9e935d550
junw <45444154+wo1261931780@users.noreply.github.com>
2023-03-15 15:05:19 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>home</nav>
+  <div>
+    <demo-header></demo-header>
+  </div>
+  <router-view />
+</template>
+
+<style></style>
+<script>
+import demoHeader from "@/components/demoHeader.vue";
+
+export default {
+  name: "layout-demo",
+  components: {
+    demoHeader,
+  },
+};
+</script>
---
+++ src/components/demoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>demo</div>
+    <div>show me</div>
+    <el-button>我是按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demoHeader",
+};
+</script>
+
+<style scoped></style>
